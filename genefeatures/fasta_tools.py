062b84ac73f3fb4cd37c17f7c0c784b2fe339ae6
Yogiski <cyogodzi@gmail.com>
2024-05-15 21:17:54 -0400

    final flake8 changes

--- genefeatures/fasta_tools.py
+++ genefeatures/fasta_tools.py
@@ -4,7 +4,12 @@ import subprocess
 def extract_sequence(fasta_file, chromosome, start, end):
 
     command = f"samtools faidx {fasta_file} {chromosome}:{start}-{end}"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    result = subprocess.run(
+        command,
+        shell=True,
+        capture_output=True,
+        text=True
+    )
     if result.returncode == 0:
         return result.stdout.split('\n', 1)[1].replace('\n', '')
     else:
